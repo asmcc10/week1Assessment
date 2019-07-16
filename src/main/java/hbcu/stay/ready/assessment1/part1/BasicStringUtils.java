df1c9861794c220f61100486dbc1f3692e0ec61d
asmcc10 <asmcc10@morgan.edu>
2019-07-16 06:50:51 -0400

    Part one strings

--- src/main/java/hbcu/stay/ready/assessment1/part1/BasicStringUtils.java
+++ src/main/java/hbcu/stay/ready/assessment1/part1/BasicStringUtils.java
@@ -9,7 +9,15 @@ public class BasicStringUtils {
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+        String[] words = str.split(" ");
+
+
+        String newString = "";
+        for(int i = 0; i <= words.length-1;i++){
+            newString = words[i].substring(0,1).toUpperCase() + words[i].substring(1) + "";
+        }
+
+        return newString;
     }
 
     /**
@@ -17,22 +25,52 @@ public class BasicStringUtils {
      * @return string with identical contents, in the reverse order
      */
     public static String reverse(String str) {
-        return null;
-    }
+
+        String reversedString = "";
+        for(int i=str.length()-1; i >= 0; i--){
+            reversedString = reversedString + str.charAt(i);
+        }
+
+        return reversedString;
+        }
 
     /**
      * @param str string input from client
      * @return string with identical contents, with each word individually in reverse order
      */
+
     public static String reverseWords(String str) {
-        return null;
+
+        String[] array = str.split(" ");
+
+        String reversedString = "";
+        for(int x = 0; x < array.length; x++){
+            String word = " " + array[x];
+            for(int i=word.length()-1; i >= 0; i--){
+                reversedString = reversedString + word.charAt(i);
+        }
+        }
+
+        return reversedString;
     }
 
     /**
      * @param str string input from client
      * @return string with identical contents, in reverse order, with first character capitalized
      */
+
     public static String reverseThenCamelCase(String str) {
+        String[] array = str.split(" ");
+
+        String reversedString = "";
+        for(int x = 0; x < array.length; x++){
+            String word = " " + array[x];
+            for(int i=word.length()-1; i >= 0; i--){
+                reversedString = reversedString + word.charAt(i);
+            }
+        }
+
+
         return null;
     }
 
@@ -41,15 +79,23 @@ public class BasicStringUtils {
      * @param str a string input from user
      * @return string with identical contents excluding first and last character
      */
+
     public static String removeFirstAndLastCharacter(String str) {
-        return null;
+        StringBuilder deleteChar = new StringBuilder(str);
+
+        deleteChar.deleteCharAt(0);
+        deleteChar.deleteCharAt(str.length()-1);
+
+        return deleteChar.toString();
     }
 
     /**
      * @param str a string input from user
      * @return string with identical characters, each with opposite casing
      */
+
     public static String invertCasing(String str) {
+
         return null;
     }
 
